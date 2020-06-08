dd1eef64413c55fc5a1513c10f5762255f6fdb35
Kyle Bittinger <kylebittinger@gmail.com>
2020-06-08 10:13:48 -0400

    Add test for selectcol subcommand

--- okfastalib/test/test_command.py
+++ okfastalib/test/test_command.py
@@ -4,22 +4,36 @@ import tempfile
 from okfastalib.command import (
     okfasta_main, msa_ok_main,
 )
+from okfastalib.fasta import parse_fasta
 
 data_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
+
+small_fp = os.path.join(data_dir, "small.fasta")
+small_aligned_fp = os.path.join(data_dir, "small_aligned.fasta")
+
 rumino_fp = os.path.join(data_dir, "ruminococcus.fasta")
 rumino_aligned_fp = os.path.join(data_dir, "ruminococcus_aligned.fasta")
 
-def test_colstats():
+
+def run_msa_ok(argv, input_fp):
     output_file = tempfile.NamedTemporaryFile(mode='w+t')
-    argv = [
-        "colstats",
-        "--input", rumino_aligned_fp,
-        "--output", output_file.name]
+    argv.extend(["--input", input_fp, "--output", output_file.name])
     msa_ok_main(argv)
-    output_file.seek(0)    
+    output_file.seek(0)
+    res = output_file.readlines()
+    return res
 
-    header_line = output_file.readline()
-    assert header_line.startswith("column_position\t")
+def test_colstats():
+    output = run_msa_ok(["colstats"], rumino_aligned_fp)
 
-    col1_line = output_file.readline()
-    assert col1_line == "1\t1\t0.90\t0.0000\tT\t1.00\n"
+    assert output[0].startswith("column_position\t")
+    assert output[1] == "1\t1\t0.90\t0.0000\tT\t1.00\n"
+
+def test_selectcol_subcommand():
+    column_file = tempfile.NamedTemporaryFile(mode="w+t")
+    column_file.write("1\n6\n8\n")
+    column_file.seek(0)
+
+    output = run_msa_ok(["selectcol", column_file.name], small_aligned_fp)
+
+    assert list(parse_fasta(output)) == [('a|b 42', '-CA'), ('c|2.1 d', 'GCG')]
