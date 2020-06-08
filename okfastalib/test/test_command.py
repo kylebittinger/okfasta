7d85c4356bfcf5592178538fd2927df65a23a7e5
Kyle Bittinger <kylebittinger@gmail.com>
2020-06-08 12:59:03 -0400

    Add test for filterids subcommand

--- okfastalib/test/test_command.py
+++ okfastalib/test/test_command.py
@@ -6,34 +6,58 @@ from okfastalib.command import (
 )
 from okfastalib.fasta import parse_fasta
 
-data_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
+small_fasta = """\
+>a|b 42
+GCAGACGATAC
+>c|2.1 d
+GCAGCCGGT
+"""
 
-small_fp = os.path.join(data_dir, "small.fasta")
-small_aligned_fp = os.path.join(data_dir, "small_aligned.fasta")
+small_aligned_fasta = """\
+>a|b 42
+-CAGACGAT-
+>c|2.1 d
+GC-GCCGG--
+"""
 
-rumino_fp = os.path.join(data_dir, "ruminococcus.fasta")
-rumino_aligned_fp = os.path.join(data_dir, "ruminococcus_aligned.fasta")
+def parse_fasta_list(f):
+    return list(parse_fasta(f))
 
+def tempfile_containing(contents):
+    f = tempfile.NamedTemporaryFile(mode="w+t")
+    f.write(contents)
+    f.seek(0)
+    return f
 
-def run_msa_ok(argv, input_fp):
-    output_file = tempfile.NamedTemporaryFile(mode='w+t')
-    argv.extend(["--input", input_fp, "--output", output_file.name])
+def run_msa_ok(argv, input_data):
+    input_file = tempfile_containing(input_data)
+    output_file = tempfile_containing("")
+    argv.extend(["--input", input_file.name, "--output", output_file.name])
     msa_ok_main(argv)
     output_file.seek(0)
     res = output_file.readlines()
     return res
 
 def test_colstats():
-    output = run_msa_ok(["colstats"], rumino_aligned_fp)
-
+    output = run_msa_ok(["colstats"], small_aligned_fasta)
     assert output[0].startswith("column_position\t")
-    assert output[1] == "1\t1\t0.90\t0.0000\tT\t1.00\n"
+    assert output[1] == "1\t1\t0.50\t0.0000\tG\t1.00\n"
 
 def test_selectcol_subcommand():
-    column_file = tempfile.NamedTemporaryFile(mode="w+t")
-    column_file.write("1\n6\n8\n")
-    column_file.seek(0)
-
-    output = run_msa_ok(["selectcol", column_file.name], small_aligned_fp)
-
+    column_file = tempfile_containing("1\n6\n8\n")
+    output = run_msa_ok(["selectcol", column_file.name], small_aligned_fasta)
     assert list(parse_fasta(output)) == [('a|b 42', '-CA'), ('c|2.1 d', 'GCG')]
+
+def run_okfasta(argv, input_data):
+    input_file = tempfile_containing(input_data)
+    output_file = tempfile_containing("")
+    argv.extend(["--input", input_file.name, "--output", output_file.name])
+    okfasta_main(argv)
+    output_file.seek(0)
+    res = output_file.readlines()
+    return res
+
+def test_filterids_subcommand():
+    ids_file = tempfile_containing("c|2.1")
+    output = run_okfasta(["filterids", ids_file.name], small_fasta)
+    assert parse_fasta_list(output) == [("c|2.1 d", "GCAGCCGGT")]
