109dcc1a07c28b45fec944bd2fbcbe468d589522
Kyle Bittinger <kylebittinger@gmail.com>
2022-11-15 11:54:42 -0500

    Simplified FASTA parsing

--- okfastalib/fasta.py
+++ okfastalib/fasta.py
@@ -1,39 +1,21 @@
 from io import StringIO
 
-def parse_fasta(f, trim_desc=False):
-    """Parse a FASTA format file.
-
-    Parameters
-    ----------
-    f : File object or iterator returning lines in FASTA format.
-
-    Returns
-    -------
-    An iterator of tuples containing two strings
-        First string is the sequence description, second is the
-        sequence.
-
-    Notes
-    -----
-    This function removes whitespace in the sequence and translates
-    "U" to "T", in order to accommodate FASTA files downloaded from
-    SILVA and the Living Tree Project.
-    """
+def parse_fasta(f):
     f = iter(f)
-    desc = next(f).strip()[1:]
-    if trim_desc:
-        desc = desc.split()[0]
+    line = next(f)
+    line = line.strip()
+    assert line.startswith(">")
+    desc = line[1:]
     seq = StringIO()
     for line in f:
         line = line.strip()
         if line.startswith(">"):
             yield desc, seq.getvalue()
             desc = line[1:]
-            if trim_desc:
-                desc = desc.split()[0]
             seq = StringIO()
         else:
-            line = line.replace(" ", "").replace("U", "T").replace(".", "-")
+            # Whitespace is not meaningful
+            line = line.replace(" ", "")
             seq.write(line)
     yield desc, seq.getvalue()
 
